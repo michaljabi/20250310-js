29817a81f12f000f2b2d143e4d76842d6dcd5150
Jabi <michal.jabi@gmail.com>
2025-03-10 15:42:58 +0100

    task(a202): solved

--- JS-teoria-zadania/A-the-language-intro/a202-functions-with-parameters.js
+++ JS-teoria-zadania/A-the-language-intro/a202-functions-with-parameters.js
@@ -15,56 +15,81 @@
  */
 
 // Deklaracja:
-function sumTwoNumbers(a = 0, b = 0) {
-	console.log(a);
-	console.log(b);
-	return a + b
+function sumThreeNumbers(a = 0, b = 0, c = 0) {
+	//console.log(a);
+	//console.log(b);
+	// if(a === 100) {
+	// 	return a * b;
+	// }
+
+	// console.log(a);
+	//return [a + b, a - b];
+	return a + b + c;
 }
 
+// console.log(sumTwoNumbers(2, 3))
 
+//console.log(sumTwoNumbers(100, 3))
 
-console.log(sumTwoNumbers())
-console.log(sumTwoNumbers(1))
-console.log(sumTwoNumbers(1, 2))
-console.log(sumTwoNumbers(1, 2, 3))
-console.log(sumTwoNumbers(1, 2, 3, 8))
-
-
-
-
-
-
-
+// console.log(sumTwoNumbers())
+// console.log(sumTwoNumbers(1))
+// console.log(sumTwoNumbers(1, 2))
+// console.log(sumTwoNumbers(1, 2, 3))
+// console.log(sumTwoNumbers(1, 2, 3, 8))
 
 
 
 // Wywołanie:
-// const result = sumTwoNumbers(10, 20)
+const result = sumThreeNumbers(10, 20)
 
 
 // #1 Zadanie:
 // Pokaż wynik dodawania 10 + 20 na ekranie
-
+console.log(result);
+console.log(sumThreeNumbers(10, 20));
 
 // #2 Zadanie:
 // Policz ile to 30 + 560
-
+console.log(sumThreeNumbers(30, 560));
 
 // #3 Zadanie:
 // co jeśli byśmy mieli policzyć 450 + 200 + 100 ?
+console.log(sumThreeNumbers(sumThreeNumbers(450, 200), 100));
 
+
+const partialSum = sumThreeNumbers(450, 200);
+console.log(sumThreeNumbers(partialSum, 100));
 
 
 /* ---------------------- */
 // Funkcje po 2015 roku mogą posiadać parametry domyślne:
 
-function greetings(user = 'Admin') {
-	console.log('Hello', user)
+function greetings(name = 'Admin', lastName) {
+	console.log('Hello', name, lastName)
 }
+
+// ZAMIAST TAK:
+// function greetings(name = 'Admin', lastName) {
+
+// PISZ TAK:
+// function greetings(lastName, name = 'Admin') {
+
+// tzn. umieszczaj domyślne parametry na końcu.
+
+
 
 // #4 Zadanie:
 // spróbuj wywołać greetings z argumentami i bez argumentu, co się stanie ?
-
+greetings(undefined, 'Kowalski')
+// greetings('Michał')
 
 // #5 Zadanie:
 // Po wykonaniu zadania #4 usuń domyślną wartość user - co się stanie ?
+
+
+// Pytanie: czy w JS są tzw. named arguments
+// a'la: greetings(lastName: '', user: )  ???
+
+// TODO 
+// Odpowieź: NIE, ale można je "zasymulować" używając obiektów:
+// Pokaże w przyszłośći
