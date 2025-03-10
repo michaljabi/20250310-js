5b0b7a4b7eae7b3a2babc2f7fcab966b4ac3af76
Jabi <michal.jabi@gmail.com>
2025-03-10 15:52:32 +0100

    task(a202): make it work

--- JS-teoria-zadania/A-the-language-intro/a202-train-functions.js
+++ JS-teoria-zadania/A-the-language-intro/a202-train-functions.js
@@ -3,3 +3,15 @@
 // wywołana bez argumentu
 // wywołana z argumentem 'Michał' wyświetli napis: 'Witaj Michał'
 // Zastanów się nad poprawnymi nazwami funkcji i jej parametru
+
+
+// 1. Make it work
+// 2. Make it right
+// 3. Make it fast
+
+function zz(name = 'Nieznajomy') {
+    return 'Witaj ' + name;
+} 
+
+console.log(zz())
+console.log(zz('Michał'))
