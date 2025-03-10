9434971c9b92c0c75dc28b79613584935a8e64da
Jabi <michal.jabi@gmail.com>
2025-03-10 15:57:49 +0100

    explain: approach to coding in general (3 phases).

--- JS-teoria-zadania/A-the-language-intro/a202-train-functions.js
+++ JS-teoria-zadania/A-the-language-intro/a202-train-functions.js
@@ -5,9 +5,9 @@
 // Zastanów się nad poprawnymi nazwami funkcji i jej parametru
 
 
-// 1. Make it work
-// 2. Make it right
-// 3. Make it fast
+// 1. Make it work // działający prototyp
+// 2. Make it right // pochylenie się nad kodem i tzw. refactoring (dobre praktyki i "czyste" rozwiązanie)
+// 3. Make it fast // próba optymalizacji rozwiązania
 
 function sayHelloTo(name = 'Nieznajomy') {
     console.log('Witaj', name);
