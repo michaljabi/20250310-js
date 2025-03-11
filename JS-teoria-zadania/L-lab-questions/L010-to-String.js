b775df134f76e9b9e03c89fa31846ca991735360
Jabi <michal.jabi@gmail.com>
2025-03-11 11:44:54 +0100

    explain(L010): toString for numbers + random string, radix and crypto (for random UUID)

---
+++ JS-teoria-zadania/L-lab-questions/L010-to-String.js
@@ -0,0 +1,25 @@
+// import biblioteki w Node.js (tzw. CommonJS) - wymyślone w 2009.
+const crypto = require('crypto')
+
+console.log(1010);
+
+console.log(1010..toString());
+console.log((1010).toString());
+
+console.log((2).toString(2));
+console.log((2).toString(8));
+console.log((2).toString(16));
+
+console.log((100).toString(16));
+
+// random number to random string
+console.log((81253876).toString(36));
+console.log((81253876).toString(36));
+
+
+// random strings:
+console.log(crypto.randomUUID())
+console.log(crypto.randomUUID())
+console.log(crypto.randomUUID())
+console.log(crypto.randomUUID())
+console.log(crypto.randomUUID())
