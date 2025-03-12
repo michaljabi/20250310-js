86d63289c0bacc998eb7282d3738eadf485c4374
Jabi <michal.jabi@gmail.com>
2025-03-12 14:22:06 +0100

    explain(eslint): no-var, no-shadow rules.

--- JS-teoria-zadania/B-modular-js-in-node/my-first-node-js-project/src/main.js
+++ JS-teoria-zadania/B-modular-js-in-node/my-first-node-js-project/src/main.js
@@ -1,10 +1,10 @@
-import { divideBy, MathError } from "./app/math-operations.js";
-import { makeBanner } from "./make-banner.js";
+import { divideBy, MathError } from './app/math-operations.js'
+import { makeBanner } from './make-banner.js'
 
 // import NN, { TEST as myTest, hello } from "./make-banner.js"; // import default i alias importu named.
 // import * as collection from "./make-banner.js"; // namespace import
 
-console.log('Witaj w BannerShow');
+console.log('Witaj w BannerShow')
 
 makeBanner('To jest mój testowy banner')
 // console.log(collection.default)
@@ -13,21 +13,22 @@ makeBanner('To jest mój testowy banner')
 console.log('------')
 
 try {
-    // s
-    console.log('divide 100 / 10 =', divideBy(100, 10))
-    console.log('divide 100 / 2 =', divideBy(100, 2))
-    console.log('divide 56 / 0 =', divideBy(56, 0))
+  // s
+  console.log('divide 100 / 10 =', divideBy(100, 10))
+  console.log('divide 100 / 2 =', divideBy(100, 2))
+  console.log('divide 56 / 0 =', divideBy(56, 0))
 } catch (e) {
-    if(e instanceof ReferenceError) {
-        console.warn('ReferenceError error happend', e.message)
-    } else if(e instanceof MathError) {
-        console.log('Math error:', e.message)
-    } else if(e instanceof Error) {
-        console.log(e)
-    } else {
-        console.log('Unhandled, unknown... error', e)
-    }
+  if (e instanceof ReferenceError) {
+    console.warn('ReferenceError error happend', e.message)
+  } else if (e instanceof MathError) {
+    console.log('Math error:', e.message)
+  } else if (e instanceof Error) {
+    console.log(e)
+  } else {
+    console.log('Unhandled, unknown... error', e)
+  }
 } finally {
-    let x = 10
-    console.log('I will always run...', x)
+  // przykład es-lint errora:
+  let makeBanner = 10
+  console.log('I will always run...', makeBanner)
 }
