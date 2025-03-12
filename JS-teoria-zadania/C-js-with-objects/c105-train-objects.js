3eae2ad8e240f8ad016735ccf661c833a5e7f0e5
Jabi <michal.jabi@gmail.com>
2025-03-12 09:29:19 +0100

    task(c105): solved - make it work

--- JS-teoria-zadania/C-js-with-objects/c105-train-objects.js
+++ JS-teoria-zadania/C-js-with-objects/c105-train-objects.js
@@ -12,12 +12,40 @@ function greetings() {
 	 mają one mieć te same wartości co rzeczy zadeklarowane powyżej
 */
 
+const shop = {
+	fruits: fruits, // tak nie trzeba bo pola mojego obiektu mają te same nazwy co identifier wyżej
+	name,
+	greetings
+}
+
+console.log(shop)
+
 // #2 Zadanie:
 // pokaż na konsoli napis "banana welcome!" używając danych z obiektu
+console.log(shop.fruits[2], shop.greetings())
+
 
 // #3 Zadanie:
 // utwórz 2 obiekt concurrentShop który będzie klonem poprzedniego (zadeklaruj z tymi samymi wartościami)
 // dodatkowo będzie miał również metodę banner - która zwróci "here prices are the lowest!"
+const concurrentShop = {
+	fruits,
+	name,
+	greetings,
+	banner() {
+		return "here prices are the lowest!"
+	}
+}
 
 // #4 Zadnie:
 // Pokaż na konsoli wszystkie nazwy pól obiektu shop
+// 1:
+for(const key in shop) {
+	console.log(key)
+}
+// 2:
+console.log(Object.keys(shop))
+// console.log(Object.values(shop))
+console.log(Object.keys(concurrentShop))
+
+export {};
