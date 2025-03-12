daae1f964549f96ca825cfbedc486a13f6a0c314
Jabi <michal.jabi@gmail.com>
2025-03-12 12:43:06 +0100

    task(my-first-node-project): 11-14 solved

--- JS-teoria-zadania/B-modular-js-in-node/my-first-node-js-project/src/main.js
+++ JS-teoria-zadania/B-modular-js-in-node/my-first-node-js-project/src/main.js
@@ -1,3 +1,4 @@
+import { divideBy, MathError } from "./app/math-operations.js";
 import { makeBanner } from "./make-banner.js";
 
 // import NN, { TEST as myTest, hello } from "./make-banner.js"; // import default i alias importu named.
@@ -9,3 +10,23 @@ makeBanner('To jest mój testowy banner')
 // console.log(collection.default)
 // console.log(myTest)
 
+console.log('------')
+
+try {
+    // s
+    console.log('divide 100 / 10 =', divideBy(100, 10))
+    console.log('divide 100 / 2 =', divideBy(100, 2))
+    console.log('divide 56 / 0 =', divideBy(56, 0))
+} catch (e) {
+    if(e instanceof ReferenceError) {
+        console.warn('ReferenceError error happend', e.message)
+    } else if(e instanceof MathError) {
+        console.log('Math error:', e.message)
+    } else if(e instanceof Error) {
+        console.log(e)
+    } else {
+        console.log('Unhandled, unknown... error', e)
+    }
+} finally {
+
+}
